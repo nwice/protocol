4cc08b06d03cb7bbd28ff2ba5b24a85400ba39b4
theabominablesasquatch <79382337+theabominablesasquatch@users.noreply.github.com>
2021-03-17 22:42:30 -0700

    Create strategy-zero-wavax-zero-lp.sol
    Unverified pointer addresses. Seems accurate from initial search

---
+++ contracts/strategies/zero/strategy-zero-wavax-zero-lp.sol
@@ -0,0 +1,34 @@
+// SPDX-License-Identifier: MIT
+pragma solidity ^0.6.7;
+
+import "../strategy-zero-farm-base.sol";
+
+contract StrategyZeroAvaxZeroLp is StrategyZeroFarmBase {
+    // Token addresses
+    address public zero_avax_zero_rewards = 0x60F19487bdA9c2F8336784110dc5c4d66425402d;
+    address public zero_avax_zero_lp = 0x0751f9a49d921aa282257563c2041b9a0e00eb78;
+   
+    constructor(
+        address _governance,
+        address _strategist,
+        address _controller,
+        address _timelock
+    )
+        public
+        StrategyZeroFarmBase(
+            zero,
+            zero_avax_zero_rewards,
+            zero_avax_zero_lp,
+            _governance,
+            _strategist,
+            _controller,
+            _timelock
+        )
+    {}
+
+    // **** Views ****
+
+    function getName() external override pure returns (string memory) {
+        return "StrategyZeroAvaxZeroLp";
+    }
+}
